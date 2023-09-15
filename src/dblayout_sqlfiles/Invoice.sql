341ea2576ab5fe0ecbe2f994a134f6409a5beb3b
Ujjwal Gupta <82048242+ujjwa893@users.noreply.github.com>
2023-09-15 06:57:59 +0530

    Add files via upload

---
+++ src/dblayout_sqlfiles/Invoice.sql
@@ -0,0 +1,7 @@
+insert into invoice values(101,"2022-05-29",1,"inter-state");
+insert into invoice values(102,"2023-04-20",0,"SameState");
+insert into invoice values(103,"2022-06-19",1,"inter-state");
+insert into invoice values(104,"2023-03-24",0,"SameState");
+insert into invoice values(105,"2022-01-12",1,"inter-state");
+select * from invoice; 
+
